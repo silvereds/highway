cce4d57f16c7327f01ccfca5a21f80e0bbe19f49
DELL <tanzebright@gmail.com>
2020-11-03 20:05:31 +0100

    Fix || Fix button padding

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -59,7 +59,7 @@ class SplashScreen extends StatelessWidget {
                       Text(
                         "Send upto 50,000FCFA to your loved ones without charges. Oh ye, it is free!! ",
                         textAlign: TextAlign.left,
-                        style: TextStyle(color: Colors.grey, fontSize: 12),
+                        style: TextStyle(color: Colors.grey, fontSize: 12, height: 1.6),
                       ),
                     ],
                   ),
@@ -95,7 +95,7 @@ class SplashScreen extends StatelessWidget {
                       Text(
                         "Manage your cards, your card readers. Assign them to your loved ones,to your acquaintances as you please and follow in real time all the transactions made on/with your devices. ",
                         textAlign: TextAlign.left,
-                         style: TextStyle(color: Colors.grey, fontSize: 12),
+                         style: TextStyle(color: Colors.grey, fontSize: 12, height: 1.6),
                       ),
                     ],
                   ),
@@ -133,7 +133,7 @@ class SplashScreen extends StatelessWidget {
                       Text(
                         "View your balance and transaction history. Manage your expenses and revenues.Recharge your acccounts via your mobile money accounts(MTN,Orange,Express Union, etc) or directly via a HighWeh branch or even at a private individual near you.Make your cash withdrawals directly from a HighWeh branch or from an individual near you  ",
                         textAlign: TextAlign.left,
-                        style: TextStyle(color: Colors.grey, fontSize: 12),
+                        style: TextStyle(color: Colors.grey, fontSize: 12, height: 1.6),
                       ),
                     ],
                   ),
@@ -142,7 +142,7 @@ class SplashScreen extends StatelessWidget {
                   height: 16,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(5.0),
                   child: Container(
                     child: FlatButton(
                       minWidth: 133,
