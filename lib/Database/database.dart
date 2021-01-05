e72c61af8cba27c11ce1977be7fa4e30fe2331d1
DELL <tanzebright@gmail.com>
2021-01-05 12:44:55 +0100

    add database dependencies

---
+++ lib/Database/database.dart
@@ -0,0 +1,7 @@
+// simple example to retrive and save employee data from SQFlite 
+
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart'; 
