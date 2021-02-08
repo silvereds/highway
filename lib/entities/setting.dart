ab13e4dde1d9d695db26bef3868a1175eab57bb6
DELL <tanzebright@gmail.com>
2021-02-08 15:13:45 +0100

    initial commit

--- lib/entities/setting.dart
+++ lib/entities/setting.dart
@@ -1,35 +1,22 @@
- class Setting {
+class Setting {
   String id;
   String name;
   String value;
   String defaultValue;
 
-
-
-
-  Setting({
-    this.id, 
-    this.name,
-    this.value,
-    this.defaultValue
-  }); 
-
-
+  Setting({this.id, this.name, this.value, this.defaultValue});
 
   factory Setting.fromJson(Map<String, dynamic> json) => Setting(
-      id: json['id'] as String,
-      name: json['json'] as String, 
-      value: json['value'] as String, 
-      defaultValue: json['defaultValue'] as String,   
-  ); 
-
-
+        id: json['id'] as String,
+        name: json['json'] as String,
+        value: json['value'] as String,
+        defaultValue: json['defaultValue'] as String,
+      );
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'name': name,
-    'value': value,
-    'defaultValue': defaultValue,
-
-  } ;
+        'id': id,
+        'name': name,
+        'value': value,
+        'defaultValue': defaultValue,
+      };
 }
