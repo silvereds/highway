abaa81fd8a39d3ed276baf692f63e735d05bac64
DELL <tanzebright@gmail.com>
2021-02-08 12:16:58 +0100

    Fix bottomNavigation

--- lib/shared/bottomNavigationBar.dart
+++ lib/shared/bottomNavigationBar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/shared/routes.dart';
+
+import 'package:mobile/views/all_accoutsView.dart';
+import 'package:mobile/views/dashboard.dart';
+import 'package:mobile/views/transactions.dart';
 
 class BottomNavigationBarView extends StatefulWidget {
   @override
@@ -10,75 +13,88 @@ class BottomNavigationBarView extends StatefulWidget {
 class _BottomNavigationBarViewState extends State<BottomNavigationBarView> {
   int _currentIndex = 0;
 
-  // static const List<Widget> _widgetOption = <Widget> [
+    final  List<Widget> _children = <Widget> [
+      DashBoard(),
+      AccountsView(),
+      TransactionsView(),
+      DashBoard(),
+      AccountsView(),
+    
+
 
  
 
-  // ];
+  ];
  
 
-  getBody() {
-    if (this._currentIndex == 0) {
-      Navigator.pushNamed(context, AppRoutes.loginPage);
-    } else if (this._currentIndex == 1) {
-       Navigator.pushNamed(context, AppRoutes.loginPage);
-    } else {
-      Navigator.pushNamed(context, AppRoutes.loginPage);
-    }
-  }
+  // getBody() {
+  //   if (this._currentIndex == 0) {
+  //     Navigator.pushNamed(context, AppRoutes.loginPage);
+  //   } else if (this._currentIndex == 1) {
+  //      Navigator.pushNamed(context, AppRoutes.loginPage);
+  //   } else {
+  //     Navigator.pushNamed(context, AppRoutes.loginPage);
+  //   }
+  // }
 
-  void onTapHandler(int index) {
-    this.setState(() {
-      this._currentIndex = index;
-    });
-    getBody();
-  }
+  // void onTapHandler(int index) {
+  //   this.setState(() {
+  //     this._currentIndex = index;
+  //   });
+  //   getBody();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: _currentIndex,
-      selectedFontSize: 18,
-      backgroundColor: Color(0xFFFFFFFF),
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Color(0xFF53B27F),
-      unselectedItemColor: Color(0xFF334D6E),
-      onTap: (int index) {
-       // Respond to item press.
-       this.onTapHandler(index);
-      },
-      items: [
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.dashboard_outlined,
-              size: 30,
-            ),
-            label: ('Dashboad')),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.credit_card,
-              size: 30,
-            ),
-            label: 'Accounts'),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.local_atm_outlined,
-              size: 30,
-            ),
-            label: 'Transactions'),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.credit_card,
-              size: 30,
-            ),
-            label: 'Devices'),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.place_outlined,
-              size: 30,
-            ),
-            label: 'Map'),
-      ],
+    return Scaffold(
+          body: _children[_currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedFontSize: 18,
+        backgroundColor: Color(0xFFFFFFFF),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Color(0xFF53B27F),
+        unselectedItemColor: Color(0xFF334D6E),
+        onTap: onTapTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.dashboard_outlined,
+                size: 30,
+              ),
+              label: ('Dashboad')),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.credit_card,
+                size: 30,
+              ),
+              label: 'Accounts'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.local_atm_outlined,
+                size: 30,
+              ),
+              label: 'Transactions'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.credit_card,
+                size: 30,
+              ),
+              label: 'Devices'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.place_outlined,
+                size: 30,
+              ),
+              label: 'Map'),
+        ],
+      ),
     );
   }
+
+void onTapTapped(int index){
+  setState(() {
+    _currentIndex = index;
+  });
+}
 }
