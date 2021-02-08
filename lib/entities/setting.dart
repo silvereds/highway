921505e4f7d95367173aeb35358a614299aa9f18
DELL <tanzebright@gmail.com>
2021-02-08 15:08:50 +0100

    Add settings model

---
+++ lib/entities/setting.dart
@@ -0,0 +1,35 @@
+ class Setting {
+  String id;
+  String name;
+  String value;
+  String defaultValue;
+
+
+
+
+  Setting({
+    this.id, 
+    this.name,
+    this.value,
+    this.defaultValue
+  }); 
+
+
+
+  factory Setting.fromJson(Map<String, dynamic> json) => Setting(
+      id: json['id'] as String,
+      name: json['json'] as String, 
+      value: json['value'] as String, 
+      defaultValue: json['defaultValue'] as String,   
+  ); 
+
+
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+    'value': value,
+    'defaultValue': defaultValue,
+
+  } ;
+}
