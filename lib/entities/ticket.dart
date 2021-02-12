704f938ff1ffe9bb342a6db2696154105f4aab6a
DELL <tanzebright@gmail.com>
2021-02-12 14:56:02 +0100

    Add notification model

--- lib/entities/ticket.dart
+++ lib/entities/ticket.dart
@@ -1,44 +1,35 @@
 class Ticket {
-  String  id;
-  String  openedOn;
-  String  topic;
-  String  openedBy;
-  String  handledBy; //must be operator
-  String  status;
-
+  String id;
+  String openedOn;
+  String topic;
+  String openedBy;
+  String handledBy; //must be operator
+  String status;
 
   Ticket({
     this.id,
     this.openedOn,
-
     this.topic,
-        this.openedBy,
+    this.openedBy,
     this.handledBy,
     this.status,
   });
 
-factory Ticket.fromJson(Map<String, dynamic> json) => Ticket(
-     
-     id: json['id'] as String,
-     openedOn:  json['openedOn'] as String, 
-     topic:  json['topic'] as String, 
-     openedBy:  json['openedBy'] as String, 
-     handledBy:  json['handledBy'] as String, 
-     status:  json['status'] as String, 
-      
-
-) ;
-
-
+  factory Ticket.fromJson(Map<String, dynamic> json) => Ticket(
+        id: json['id'] as String,
+        openedOn: json['openedOn'] as String,
+        topic: json['topic'] as String,
+        openedBy: json['openedBy'] as String,
+        handledBy: json['handledBy'] as String,
+        status: json['status'] as String,
+      );
 
   Map<String, dynamic> toJson() => {
-     'id': id,
-     'openedOn': openedOn, 
-     'topic': topic,
-     'openedBy': openedBy, 
-     'handledBy': handledBy,
-     'status': status
-  };
- 
-
-  }
+        'id': id,
+        'openedOn': openedOn,
+        'topic': topic,
+        'openedBy': openedBy,
+        'handledBy': handledBy,
+        'status': status
+      };
+}
