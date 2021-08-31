a42f560459439d372550f907486482cea15f82e8
DELL <tanzebright@gmail.com>
2021-08-31 06:35:50 +0100

    Add google map api

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyC3NqYIU5egq_7E3PEcQZLWiVdrX9-hrzs")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
