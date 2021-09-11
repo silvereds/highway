72bfa72d0ebbe6763bc6e81bf1d9a6e72d203a19
kamtou boris <boriskamtou@gmail.com>
2021-09-11 21:17:47 +0200

    handle state

---
+++ lib/src/core/providers/branches_notifier.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mobile/src/core/common/utils.dart';
+import 'package:mobile/src/core/entities/all.dart';
+import 'package:mobile/src/core/repository/branch_repository.dart';
+
+part 'branches_notifier.freezed.dart';
+
+@freezed
+abstract class BranchesState with _$BranchesState {
+  const BranchesState._();
+  const factory BranchesState.intial() = _Initial;
+  const factory BranchesState.loading() = _Loading;
+  const factory BranchesState.loaded(List<Branche> branches) = _Loaded;
+  const factory BranchesState.error([String message]) = _Error;
+}
+
+class BrancheNotifier extends StateNotifier<BranchesState> {
+  final BranchesRepository _branchesRepository;
+  BrancheNotifier(this._branchesRepository) : super(BranchesState.intial());
+
+  Future<void> getBranches() async {
+    try {
+      state = BranchesState.loading();
+      final listOfBranches = await _branchesRepository.getAllBranch();
+      state = BranchesState.loaded(listOfBranches);
+    } catch (e) {
+      state = BranchesState.error(parseApiError(e));
+    }
+  }
+}
